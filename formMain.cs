51b2d2ae98812bf50e91b3ebc5de9893dd4c8d32
meepvn <67948767+meepvn@users.noreply.github.com>
2021-05-02 08:34:17 +0700

    Created formMain

---
+++ formMain.cs
@@ -0,0 +1,39 @@
+namespace FreakingMath
+{
+    public partial class Form1 : Form
+    {
+        public bool isPlaying = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            FormGame fGame = new FormGame();
+            fGame.Show();
+            isPlaying = true;
+            this.Close();
+        }
+
+        private void btnHighScore_Click(object sender, EventArgs e)
+        {
+            FormHS fHS = new FormHS();
+            fHS.Show();
+            isPlaying = true;
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult DR = MessageBox.Show("Do you really want to quit?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (DR == DialogResult.No) return;
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!isPlaying) Application.Exit();
+        }
+    }
+}
