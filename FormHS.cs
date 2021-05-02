90b88b3cb7b1f495ad6c010c4235142b06235530
meepvn <67948767+meepvn@users.noreply.github.com>
2021-05-02 08:50:38 +0700

    Update FormHS.cs

--- FormHS.cs
+++ FormHS.cs
@@ -34,7 +34,7 @@ namespace FreakingMath
 
         private void btnReturn_Click(object sender, EventArgs e)
         {
-            Form1 f = new Form1();
+            formMain f = new formMain();
             f.Show();
             this.Close();
         }
